e371b957671f26e6f7eef637d894ab8f0dc9c59b
Brooke Love <savienloveb@gmail.com>
2022-04-05 22:17:35 -0700

    initial message

---
+++ script.js
@@ -0,0 +1,9 @@
+//the value of the note corresponds with the time 
+///render the note to go with the time 
+//input type of text area 
+//moment.js that parse times 
+    //current time = or less than the time at the block
+    //if current time is === to current time 
+    //if current time is > the time 
+    //if current time is greater than 
+    //w3 jquerry 
