123af5ac8c84fb078e5c2a986cdf225e0c2514d8
Brooke Love <savienloveb@gmail.com>
2022-04-07 00:15:22 -0700

    adding elements

--- script.js
+++ script.js
@@ -7,3 +7,76 @@
     //if current time is > the time 
     //if current time is greater than 
     //w3 jquerry 
+//PSEUDOCODE
+    //1. grab all HTML elements
+        //classes and ids
+    //2. get the current day and day of the week 
+        //use moment() with the id tag currentDay use folder 25 and 26
+    //3.want the current time to work in down to hour, minutes, seconds
+        //set a currentTime id  and link it to text area 
+        //compare the current hour to the previous hour
+        //write if else statemenet "if the current time is greater than the time on the screen make grey..."
+        //use data-time and time input 
+        //use military time to make it easier to compare greater than to later 
+    //4. have a save button to save it to local storage on click
+        //add event listener for save button
+            //search: how to save text area to local storage 
+            //pull text area out of local storage 
+            //push it to the text area again to make sure its reading correct 
+//WORKSPACE
+//elements
+var saveBtn = $('#save')
+var hourEl = $('#hour')
+var userInput = $('#userInput')
+var local = JSON.parse(localStorage.getItem("save"))
+var othercontent = []
+var hourplace = Number(hourEl.text)
+var timeplace = Number(moment().format('HH')) 
+// var endput= local[0]
+//current day
+//written as Thursday, September 5th
+    var currentDay = moment();
+   $('#currentDay').text(currentDay.format('dddd, MMMM Do'));
+
+
+    //  var today = $('#today').moment().format('HH');
+    //how to write the current time of and compare it to a number 
+    //write in for loop
+    //make it an object?
+     if ( timeplace < hourplace) {
+        $('.textarea').addClass('past')
+        $('.textarea').removeClass('future')
+        $('.textarea').removeClass('present')
+     } else if (timeplace === hourplace) {
+        $('.textarea').addClass('present')
+        $('.textarea').removeClass('past')
+        $('.textarea').removeClass('future')
+     } else if (timeplace > hourplace) {
+        $('.textarea').addClass('future')
+        $('.textarea').removeClass('past')
+        $('.textarea').removeClass('present')
+        
+    }
+
+function savefunc (){
+    var save = [{
+        text: $(this).siblings('textarea').val(),
+        hour: $(this).siblings('div').text()
+    }]
+    othercontent.push(save)
+    var pullingcontent = JSON.stringify(othercontent)
+    
+    if (local === null) {
+    JSON.stringify(pullingcontent)
+    localStorage.setItem("text", pullingcontent)
+
+    } else {
+    othercontent.push(JSON.parse(localStorage.getItem("text")))
+    localStorage.setItem("text", pullingcontent)
+    }
+
+}
+saveBtn.click(savefunc)
+
+
+
